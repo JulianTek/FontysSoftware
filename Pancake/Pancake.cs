301065470b9c71ac8334334cd17c40dae260370a
Julian <Julian-Tekstra@hotmail.com>
2020-04-01 14:44:48 +0200

    Created another sorting algorithm

---
+++ Pancake/Pancake.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Pancake
+{
+    public class Pancake
+    {
+        public Pancake(int size)
+        {
+            this.size = size;
+        }
+        private int size { get; set; }
+        private Pancake max { get; set; }
+
+
+        public void sort(List<Pancake> pancakes)
+        {
+            int biggest = 0;
+            List<Pancake> sortingList = new List<Pancake>();
+            int count = pancakes.Count + 1;
+            for (int i = 0; i < count; i++)
+            {
+                foreach (Pancake pancake in pancakes)
+                {
+                    if (pancake.size > biggest)
+                    {
+                        biggest = pancake.size;
+                        max = pancake;
+                    }
+                }
+                sortingList.Add(max);
+                pancakes.Remove(max);
+                biggest = 0;
+            }
+            pancakes = sortingList;
+            foreach (Pancake pancake2 in pancakes)
+            {
+                Console.WriteLine(pancake2.size);
+            }
+        }
+    }
+}
---
+++ Pancake/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Pancake
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Pancake> pancakes = new List<Pancake>();
+            Pancake pancake = new Pancake(1);
+            Pancake pancake1 = new Pancake(7);
+            Pancake pancake2 = new Pancake(5);
+            Pancake pancake3 = new Pancake(34);
+            Pancake pancake4 = new Pancake(3);
+            Pancake pancake5 = new Pancake(12);
+            Pancake pancake6 = new Pancake(100);
+            Pancake pancake7 = new Pancake(16);
+            Pancake pancake8 = new Pancake(11);
+            Pancake pancake9 = new Pancake(19);
+            pancakes.Add(pancake1);
+            pancakes.Add(pancake2);
+            pancakes.Add(pancake3);
+            pancakes.Add(pancake4);
+            pancakes.Add(pancake5);
+            pancakes.Add(pancake6);
+            pancakes.Add(pancake7);
+            pancakes.Add(pancake8);
+            pancakes.Add(pancake9);
+
+            pancake.Sort(pancakes);
+        }
+    }
+}
